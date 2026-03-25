e4b6ae2de51ffd2be1d8444409660e52bb071de1
DCluseau <daphne.cluseau@fms-ea.com>
2026-03-25 13:34:58 +0100

    Added Article and Category classes

---
+++ src/main/java/fr/fms/entities/Category.java
@@ -0,0 +1,76 @@
+package fr.fms.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "category")
+	private Collection<Article> articles;
+
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Category(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	/*
+	 * Article not displayed to avoid errors
+	 */
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
+	
+}
